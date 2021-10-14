1ced99442641dd5c20dfe7ba70b97be6a50a9e46
user0028 <89523119+user0028@users.noreply.github.com>
2021-10-15 02:20:42 +0530

    Update viz2.js

--- JS/viz2.js
+++ JS/viz2.js
@@ -1,11 +1,11 @@
-var vg_1 = "choro_death.vg.json";
-var vg_2 = "stream.vg.json";
-var vg_3 = "slope.vg.json";
-var vg_4 = "area.vg.json";
-var vg_5 = "choro_vax.vg.json";
+var vg_1 = "multi4.vg.json";
+var vg_2 = "bubble.vg.json";
+var vg_3 = "top.vg.json";
+var vg_4 = "area2.vg.json";
+var vg_5 = "choro_vax2.vg.json";
 var vg_6 = "test3.vg.json";
-var vg_7 = "gdp.vg.json";
-
+var vg_7 = "custom6.vg.json";
+var vg_8 = "custom5.vg.json";
 
 vegaEmbed("#choropleth_death", vg_1).then(function(result) {
  // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
@@ -34,3 +34,7 @@ vegaEmbed('#waffle_chart', vg_6, {"actions": false}).then(function(result) {
 vegaEmbed('#multi_line', vg_7, {"actions": false}).then(function(result) {
 // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
+
+vegaEmbed('#custom2', vg_8, {"actions": false}).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+    }).catch(console.error);
