206c817c09d337f2b41ebe867690025887524845
user0028 <89523119+user0028@users.noreply.github.com>
2021-10-16 13:24:59 +0530

    Add files via upload

---
+++ viz2.js
@@ -0,0 +1,41 @@
+var vg_1 = "JS/multi4.vg.json";
+var vg_2 = "JS/waterfall.vg.json";
+var vg_3 = "JS/top.vg.json";
+var vg_4 = "JS/area2.vg.json";
+var vg_5 = "JS/choro_vax2.vg.json";
+var vg_6 = "JS/test3.vg.json";
+var vg_7 = "JS/custom6.vg.json";
+var vg_8 = "JS/custom5.vg.json";
+
+vegaEmbed("#chart_2", vg_1).then(function(result) {
+ // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+vegaEmbed('#chart_1', vg_2, {"actions": false}).then(function(result) {
+// Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+
+vegaEmbed('#chart_5', vg_3, {"actions": false}).then(function(result) {
+// Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+vegaEmbed('#chart_4', vg_4, {"actions": false}).then(function(result) {
+// Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+vegaEmbed('#chart_3', vg_5, {"actions": false}).then(function(result) {
+// Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+vegaEmbed('#chart_6', vg_6, {"actions": false}).then(function(result) {
+// Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+vegaEmbed('#chart_7', vg_7, {"actions": false}).then(function(result) {
+// Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+vegaEmbed('#chart_8', vg_8, {"actions": false}).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+    }).catch(console.error);
