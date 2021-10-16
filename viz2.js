00e2de7265e9cc55c3d825605c4e1f4660dacc92
user0028 <89523119+user0028@users.noreply.github.com>
2021-10-16 09:51:41 +0530

    Add files via upload

--- viz2.js
+++ viz2.js
@@ -7,34 +7,35 @@ var vg_6 = "JS/test3.vg.json";
 var vg_7 = "JS/custom6.vg.json";
 var vg_8 = "JS/custom5.vg.json";
 
-vegaEmbed("#choropleth_death", vg_1).then(function(result) {
+vegaEmbed("#chart_2", vg_1).then(function(result) {
  // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
-vegaEmbed('#stream_graph', vg_2, {"actions": false}).then(function(result) {
+vegaEmbed('#chart_1', vg_2, {"actions": false}).then(function(result) {
 // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
-vegaEmbed('#heat_map', vg_3, {"actions": false}).then(function(result) {
+
+vegaEmbed('#chart_5', vg_3, {"actions": false}).then(function(result) {
 // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
-vegaEmbed('#slope_chart', vg_4, {"actions": false}).then(function(result) {
+vegaEmbed('#chart_4', vg_4, {"actions": false}).then(function(result) {
 // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
-vegaEmbed('#choropleth_vax', vg_5, {"actions": false}).then(function(result) {
+vegaEmbed('#chart_3', vg_5, {"actions": false}).then(function(result) {
 // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
-vegaEmbed('#waffle_chart', vg_6, {"actions": false}).then(function(result) {
+vegaEmbed('#chart_6', vg_6, {"actions": false}).then(function(result) {
 // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
-vegaEmbed('#multi_line', vg_7, {"actions": false}).then(function(result) {
+vegaEmbed('#chart_7', vg_7, {"actions": false}).then(function(result) {
 // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
-vegaEmbed('#custom2', vg_8, {"actions": false}).then(function(result) {
+vegaEmbed('#chart_8', vg_8, {"actions": false}).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
     }).catch(console.error);
